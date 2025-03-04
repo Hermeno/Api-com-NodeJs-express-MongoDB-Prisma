8769423fda6aa6d14381c73ef0d1706dfdee71bf
Hermeno dev <herminiomacamo6@gmail.com>
2025-03-04 12:53:32 +0200

    Adicionado duas rotas pra adicionar credito e selecionar

--- routes/private.js
+++ routes/private.js
@@ -19,6 +19,38 @@ router.get('/listar-usuarios', async (req, res) => {
 })
 
 
+router.post('/cadastrar-credito', async (req, res) => {
+    try {
+        const { user_id, valor, data_vencimento } = req.body
+        await prisma.credito.create({
+            data: {
+                user_id,
+                moeda,
+                valor,
+                referencia,
+                data_criacao
+            }
+        })
+        res.status(200).json({ message: 'Crédito cadastrado com sucesso!' })
+    } catch (error) {
+        res.status(500).json({ message: 'Falha ao cadastrar o crédito'})
+    }
+})
+
+router.get('/buscar-creditos/:user_id', async (req, res) => {
+    try {
+        const user_id = parseInt(req.params.user_id)
+        const creditos = await prisma.credito.findMany({
+            where: {
+                user_id
+            }
+        })
+        res.status(200).json({ message: 'Créditos encontrados!', creditos })
+    } catch (error) {
+        res.status(500).json({ message: 'Falha ao buscar os créditos'})
+    }
+})
+
 
 
 
