798a9afd098cbd4475f274f7ca8889a7cc18c1b4
Hermeno dev <herminiomacamo6@gmail.com>
2025-03-04 14:07:56 +0200

    Atualizado as paginas prisma e private e adicionar mais tabelas e operacoes crud no private

--- routes/private.js
+++ routes/private.js
@@ -16,7 +16,10 @@ router.get('/listar-usuarios', async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Falha no servidor'})
     }
-})
+});
+
+
+
 
 
 router.post('/cadastrar-credito', async (req, res) => {
@@ -35,7 +38,11 @@ router.post('/cadastrar-credito', async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Falha ao cadastrar o crédito'})
     }
-})
+});
+
+
+
+
 
 router.get('/buscar-creditos/:user_id', async (req, res) => {
     try {
@@ -49,10 +56,148 @@ router.get('/buscar-creditos/:user_id', async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Falha ao buscar os créditos'})
     }
+});
+
+
+
+
+router.post('/cadastrar-cambio', async (req, res) => {
+    try {
+        const { user_id, moeda_origem, moeda_destino, cotacao, total_a_cambiar, total_cambiado, numero_recibo, foto_recibo } = req.body
+        await prisma.cambio.create({
+            data: {
+                user_id,
+                moeda_origem,
+                moeda_destino,
+                cotacao,
+                total_a_cambiar,
+                total_cambiado,
+                numero_recibo,
+                foto_recibo
+            }
+        })
+        res.status(200).json({ message: 'Cambio cadastrado com sucesso!' })
+    } catch (error) {
+        res.status(500).json({ message: 'Falha ao cadastrar o cambio'})
+    }
+});
+
+
+
+
+
+router.get('/buscar-cambios/:user_id', async (req, res) => {
+    try {
+        const user_id = parseInt(req.params.user_id)
+        const cambios = await prisma.cambio.findMany({
+            where: {
+                user_id
+            }
+        })
+        res.status(200).json({ message: 'Cambios encontrados!', cambios })
+    } catch (error) {
+        res.status(500).json({ message: 'Falha ao buscar os cambios'})
+    }
+});
+
+
+
+router.post('/cadastrar-missao', async (req, res) => {
+    try {
+        const { missao, data_inicio_prevista, data_final_prevista, pais, estado, cidade, user_id, username } = req.body
+        await prisma.missao.create({
+            data: {
+                missao,
+                data_inicio_prevista,
+                data_final_prevista,
+                pais,
+                estado,
+                cidade,
+                user_id,
+                username
+            }
+        })
+        res.status(200).json({ message: 'Missão cadastrada com sucesso!' })
+    } catch (error) {
+        res.status(500).json({ message: 'Falha ao cadastrar a missão'})
+    }
+});
+
+
+
+router.get('/buscar-missao', async (req, res) => {
+    try {
+        const missaos = await prisma.missao.findMany({
+            where: {
+                user_id: req.query.user_id
+            }
+        })
+        res.status(200).json({ message: 'Missões encontradas!', missaos })
+    } catch (error) {
+        res.status(500).json({ message: 'Falha ao buscar as missões'})
+    }
 })
 
 
+  
 
+router.post('/cadastrar-despesas', async (req, res) => {
+    try {
+        const { descricao, moeda_a_debitar, data_padrao, pais, cidade, outro, numero_recibo, valor, foto_recibo, missao_id, user_id, username } = req.body
+        await prisma.despesa.create({
+            data: {
+                descricao,
+                moeda_a_debitar,
+                data_padrao,
+                pais,
+                cidade,
+                outro,
+                numero_recibo,
+                valor,
+                foto_recibo,
+                missao_id,
+                user_id,
+                username
+            }
+        })
+        res.status(200).json({ message: 'Despesa cadastrada com sucesso!' })
+    } catch (error) {
+        res.status(500).json({ message: 'Falha ao cadastrar a despesa'})
+    }
+});
+
+
+
+
+
+// router.get('/buscar-despesas', async (req, res) => {
+//     try {
+//         const despesas = await prisma.despesa.findMany({
+//             where: {
+//                 user_id: req.query.user_id
+//             }
+//         })
+//         res.status(200).json({ message: 'Despesas encontradas!', despesas })
+//     } catch (error) {
+//         res.status(500).json({ message: 'Falha ao buscar as despesas'})
+//     }
+// });
+
+
+
+router.get('/buscar-despesas', async (req, res) => {
+    try {
+        const despesas = await prisma.despesa.findMany({
+            where: {
+                missao_id: req.query.missao_id,
+                user_id: req.query.user_id
+            }
+        })
+        res.status(200).json({ message: 'Despesas encontradas!', despesas })
+    } catch (error) {
+        res.status(500).json({ message: 'Falha ao buscar as despesas'})
+    }
+})
 
 
 export default router;
